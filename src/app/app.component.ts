2a60cf48b3a95305511b6129474607370500c287
ketangote <ketan.gote@metamagic.in>
2018-01-10 13:09:22 +0530

    Amexio Demo App Based on version 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  nodeClick(data){
+    console.log(JSON.stringify(data));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {
+  AmexioFormsModule, AmexioNavModule, AmexioWidgetModule, CommonDataService,
+  DeviceQueryService
+} from "amexio-ng-extensions";
 
 
 @NgModule({
@@ -10,9 +14,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmexioFormsModule,
+    AmexioNavModule
+
   ],
-  providers: [],
+  providers: [DeviceQueryService,CommonDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
