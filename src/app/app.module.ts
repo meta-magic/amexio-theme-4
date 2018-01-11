4b9a95782534f711d89a1dd55f118b12cb65ce92
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-01-11 19:24:12 +0530

    itemselector demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {
-  AmexioFormsModule, AmexioLayoutModule, AmexioNavModule, AmexioWidgetModule, AmexioDashBoardModule, CommonDataService,
-  DeviceQueryService, AmexioChartsModule,ChartLoaderService
+  AmexioFormsModule, AmexioLayoutModule, AmexioNavModule, AmexioWidgetModule,  CommonDataService,
+  DeviceQueryService,IconLoaderService
 } from "amexio-ng-extensions";
 import {DashboardComponent} from "./pages/dashboard/dashboard.component";
 import {SampleFormComponent} from "./pages/sampleform/sampleform.component";
+
 
 
 
@@ -21,12 +22,10 @@ import {SampleFormComponent} from "./pages/sampleform/sampleform.component";
   imports: [
     BrowserModule,
     AmexioWidgetModule,
-    AmexioDashBoardModule,
-    AmexioLayoutModule,
-    AmexioChartsModule
+    AmexioLayoutModule
 
   ],
-  providers: [DeviceQueryService,CommonDataService, ChartLoaderService],
+  providers: [DeviceQueryService,CommonDataService,IconLoaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
