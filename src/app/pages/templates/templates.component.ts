5c5aabf979aba71189539e2c4081b4b7a2bc8553
Manisha1205 <manisha.boddu@metamagic.in>
2018-05-16 13:59:35 +0530

    template and email updated with  amexioimage and c-class

--- src/app/pages/templates/templates.component.ts
+++ src/app/pages/templates/templates.component.ts
@@ -1,13 +1,13 @@
 /**
  * Created by dattaram on 13/1/18.
  */
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 @Component({
   moduleId: module.id,
   selector: 'templates',
   templateUrl: 'templates.component.html',
-   styles:[`
+  styles: [`
     .listbox-StatusYellow {
       background-color: yellow;
     }
@@ -17,6 +17,7 @@ import {HttpClient} from '@angular/common/http'
     .listbox-StatusRed {
       background-color: red;
   }
+  
   
 .red {
     color: red!important;
@@ -37,12 +38,12 @@ import {HttpClient} from '@angular/common/http'
 })
 
 export class TemplatesGridComponent implements OnInit {
-  progressType:string;
-  actionWindowFlag:boolean;
-    projectData:any;
-  browserData:any;
+  progressType: string;
+  actionWindowFlag: boolean;
+  projectData: any;
+  browserData: any;
   constructor() {
- this.projectData={
+    this.projectData = {
       "response": {
         "success": true,
         "message": "Fetching  Data  Request Succeeded: Profile",
@@ -51,11 +52,12 @@ export class TemplatesGridComponent implements OnInit {
             "name": "Admin Template	",
             "status": " 65%"
           },
-          { "name": "Landing Page",
+          {
+            "name": "Landing Page",
             "status": "Finished"
           },
           {
-             "name": "Backend UI	",
+            "name": "Backend UI	",
             "status": "Rejected"
           },
           {
@@ -63,69 +65,70 @@ export class TemplatesGridComponent implements OnInit {
             "status": "40%"
           },
           {
-           "name": "E-mail Templates	",
+            "name": "E-mail Templates	",
             "status": "13%"
           },
           {
-          "name": "Corporate Website",
+            "name": "Corporate Website",
             "status": "Pending"
           }
         ]
       }
-    } 
-    this.browserData={
+    }
+    this.browserData = {
       "response": {
         "success": true,
         "message": "Fetching  Data  Request Succeeded: Profile",
         "data": [
           {
             "name": "Google Chrome",
-            "usage":"23%",
+            "usage": "23%",
             "icon": "GoogleChrome.jpg "
           },
-          { "name": "Mozila Firefox",
-            "usage":"15%",
+          {
+            "name": "Mozila Firefox",
+            "usage": "15%",
             "icon": "MozilaFirefox.png "
           },
           {
-             "name": "Apple Safari",
-             "usage":"7%",
+            "name": "Apple Safari",
+            "usage": "7%",
             "icon": "AppleSafari.png "
           },
           {
             "name": "Internet Explorer",
-            "usage":"9%",
+            "usage": "9%",
             "icon": "InternetExplorer.jpeg"
           },
           {
-           "name": "Opera mini",
-           "usage":"20%",
+            "name": "Opera mini",
+            "usage": "20%",
             "icon": "Operamini.jpg"
           },
           {
-          "name": "Microsoft edge",
-          "usage":"9%",
+            "name": "Microsoft edge",
+            "usage": "9%",
             "icon": "Microsoftedge.jpg"
           }
         ]
       }
-    } 
+    }
 
 
   }
 
   ngOnInit() {
   }
-  getGridTemplateData(row:any): string{
-    if(row.Usage>=50){
+  getGridTemplateData(row: any): string {
+    if (row.Usage >= 50) {
       return "success";
     }
-    if(row.Usage<50 && row.Usage>=30){
+    if (row.Usage < 50 && row.Usage >= 30) {
       return "warning";
     }
-       if(row.Usage<30){
-       return "danger";
+    if (row.Usage < 30) {
+      return "danger";
     }
   }
-  
+
 }
